9953e74b987f823153cfcd95d123cd801b3ba4c2
shamolmojumder <shamolmojumder14@gmail.com>
2021-08-25 01:59:12 +0600

    45-5 dynamic api, api parameter, access params, access query

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express');
 
 
 const app =express();
-
+const users=["shamol","kausar","mim","rokib","janiya","nadida"]
 app.get('/',(req,res)=>{
     const fruit ={
         product:'ada',
@@ -13,6 +13,12 @@ app.get('/',(req,res)=>{
 
 app.get('/fruits/banana',(req,res)=>{
     res.send({fruit:'banana',quantity:1000,price:10000})
+})
+app.get('/users/:id',(req,res)=>{
+    const id=req.params.id;
+    console.log(req.query.sort);
+    const name=users[id];
+    res.send({id,name})
 })
 
 app.listen(4200,()=>console.log("listening to port 4200"));
