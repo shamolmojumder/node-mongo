740ce9ed9fb13fa82a47b96a9541c1bc595fe001
shamolmojumder <shamolmojumder14@gmail.com>
2021-08-25 15:50:22 +0600

    45-7 create POST api, install postman, read post data, use body parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express =require('express');
 const cors=require('cors');
+const bodyParser = require('body-parser')
 
 const app =express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
+
 const users=["shamol","kausar","mim","rokib","janiya","nadida"]
 app.get('/',(req,res)=>{
     const fruit ={
@@ -12,14 +15,19 @@ app.get('/',(req,res)=>{
     res.send(fruit)
 })
 
-app.get('/fruits/banana',(req,res)=>{
-    res.send({fruit:'banana',quantity:1000,price:10000})
-})
+// app.get('/fruits/banana',(req,res)=>{
+//     res.send({fruit:'banana',quantity:1000,price:10000})
+// })
 app.get('/users/:id',(req,res)=>{
     const id=req.params.id;
     console.log(req.query.sort);
     const name=users[id];
     res.send({id,name})
+})
+
+// post
+app.post('/addUser',(req,res)=>{
+    console.log(req.body);
 })
 
 app.listen(4200,()=>console.log("listening to port 4200"));
