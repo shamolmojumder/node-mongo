bb912c85684dc743ceba661d98051eb140ebacf7
shamolmojumder <shamolmojumder14@gmail.com>
2021-08-25 13:57:21 +0600

    45-6 use fetch to load data from server, middleware, handle cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express =require('express');
-
+const cors=require('cors');
 
 const app =express();
+app.use(cors())
 const users=["shamol","kausar","mim","rokib","janiya","nadida"]
 app.get('/',(req,res)=>{
     const fruit ={
